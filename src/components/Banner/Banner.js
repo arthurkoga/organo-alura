67558c5d6aced1c6fb511262895fc8f9cf3903cd
Arthur Koga <arthurkoga.2b@gmail.com>
2024-09-12 10:33:12 -0300

    finalizando projeto

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,14 @@
 import './Banner.css'
 
 export const Banner = ()=>{
-
+    //JSX
     return(
+        <>
         <header className="banner">
             <img src="/imagens/banner.png" alt="O banner principal da págona do Organo"/>
         </header>
+        <h1>Teste</h1>
+        </>
     )
 }
 
